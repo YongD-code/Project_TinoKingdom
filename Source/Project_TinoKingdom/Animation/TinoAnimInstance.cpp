86feb649962d68a737a7a566fd6eaf11e990531f
Park Chan-Woong <chan081010@naver.com>
2026-07-17 02:37:14 +0900

    TinoAnimInstance의 기본적인 초기화함수, 업데이트 함수, 멤버 변수 설정

--- Source/Project_TinoKingdom/Animation/TinoAnimInstance.cpp
+++ Source/Project_TinoKingdom/Animation/TinoAnimInstance.cpp
@@ -3,3 +3,38 @@
 
 #include "TinoAnimInstance.h"
 
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+UTinoAnimInstance::UTinoAnimInstance()
+{
+	MovingSpeedThreshold = 3.f;
+	JumpingSpeedThreshold = 0.f;
+}
+
+void UTinoAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	
+	OwnerCharacter = Cast<ACharacter>(GetOwningActor());
+	if (IsValid(OwnerCharacter))
+	{
+		MovementComponent = OwnerCharacter->GetCharacterMovement();
+	}
+}
+
+void UTinoAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	
+	if (IsValid(MovementComponent))
+	{
+		Velocity = MovementComponent->Velocity;
+		GroundSpeed = Velocity.Size2D();
+		bIsIdle = GroundSpeed < MovingSpeedThreshold;
+		
+		const bool bIsInAir = MovementComponent->IsFalling();
+		bIsJumping = bIsInAir && (Velocity.Z > JumpingSpeedThreshold);
+		bIsFalling = bIsInAir && !bIsJumping;
+	}
+}
