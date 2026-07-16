134403cb41d89512ff79ed2269b998d44ccf4fcc
Park Chan-Woong <chan081010@naver.com>
2026-07-16 21:27:32 +0900

    Animation BluePrint의 부모가 될 TinoAnimInstance 생성
    실수로 또 브랜치 안 만듦

---
+++ Source/Project_TinoKingdom/Animation/TinoAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "TinoAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_TINOKINGDOM_API UTinoAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
