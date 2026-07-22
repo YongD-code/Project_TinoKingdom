05d02712a83d7a60abaae4cc0f8f47e9e3815adc
Park Chan-Woong <chan081010@naver.com>
2026-07-22 09:57:17 +0900

    CombatComponent 구현

--- Source/Project_TinoKingdom/Component/TinoCombatComponent.cpp
+++ Source/Project_TinoKingdom/Component/TinoCombatComponent.cpp
@@ -1,28 +1,308 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "TinoCombatComponent.h"
 
-// Sets default values for this component's properties
+#include "Animation/AnimInstance.h"
+#include "Animation/AnimMontage.h"
+#include "CollisionQueryParams.h"
+#include "CollisionShape.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "DrawDebugHelpers.h"
+#include "Engine/World.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/DamageType.h"
+#include "Kismet/GameplayStatics.h"
+#include "Project_TinoKingdom/Constants/TinoCollision.h"
+#include "Project_TinoKingdom/DataAsset/AttackComboData.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogTinoCombat, Log, All);
+
 UTinoCombatComponent::UTinoCombatComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+	// 공격 판정은 Anim Notify State 구간에서만 실행하므로 상시 Tick은 필요 없다.
+	PrimaryComponentTick.bCanEverTick = false;
 }
 
 void UTinoCombatComponent::InitializeCombat(USkeletalMeshComponent* InAnimationMesh)
 {
+	AnimationMesh = InAnimationMesh;
 }
 
+void UTinoCombatComponent::SetEquippedAttackData(UAttackComboData* InAttackData)
+{
+	// nullptr도 유효한 입력, 무기를 해제하면 nullptr이 전달되고 GetEffectiveAttackData()가 DefaultAttackData를 선택
+	EquippedAttackData = InAttackData;
+}
 
-// Called when the game starts
+bool UTinoCombatComponent::RequestAttack()
+{
+	UCharacterMovementComponent* MovementComponent = OwnerCharacter->GetCharacterMovement();
+	if (OwnerCharacter->bPressedJump || !MovementComponent->IsMovingOnGround())
+	{
+		return false;
+	}
+
+	UAnimInstance* AnimInstance = AnimationMesh->GetAnimInstance();
+	if (AnimInstance == nullptr)
+	{
+		UE_LOG(LogTinoCombat, Error, TEXT("%s: 공격을 재생할 AnimInstance가 없습니다."), *GetNameSafe(OwnerCharacter));
+		return false;
+	}
+
+	if (!IsAttacking())
+	{
+		return StartComboAttack(AnimInstance, ResolveAttackDataForNewAttack());
+	}
+
+	return TryQueueNextCombo(AnimInstance);
+}
+
+bool UTinoCombatComponent::IsAttacking() const
+{
+	return ActiveAttackData != nullptr;
+}
+
+void UTinoCombatComponent::SetComboInputWindowOpen(bool bIsOpen)
+{
+	bComboInputWindowOpen = bIsOpen;
+
+	if (bIsOpen)
+	{
+		// 콤보 입력창 하나에서는 다음 공격을 한 번만 예약할 수 있다.
+		bComboInputConsumed = false;
+	}
+}
+
+void UTinoCombatComponent::BeginAttackHitWindow()
+{
+	bAttackHitWindowOpen = false;
+	ActiveAttackSectionIndex = INDEX_NONE;
+	HitActorsThisWindow.Reset();
+
+	// 공격 몽타주 외의 Notify가 잘못 전달된 경우에는 판정창을 열지 않는다.
+	if (ActiveAttackData == nullptr)
+	{
+		return;
+	}
+
+	const int32 FoundSectionIndex = FindActiveComboAttackSectionIndex();
+	if (!ActiveAttackData->ComboSection.IsValidIndex(FoundSectionIndex))
+	{
+		UE_LOG(LogTinoCombat, Error, TEXT("%s: 현재 공격 섹션 데이터를 찾을 수 없습니다."), *GetNameSafe(OwnerCharacter));
+		return;
+	}
+
+	ActiveAttackSectionIndex = FoundSectionIndex;
+	bAttackHitWindowOpen = true;
+
+	// Notify 구간이 짧더라도 최소 한 번은 판정한다.
+	PerformAttackTrace();
+}
+
+void UTinoCombatComponent::TickAttackHitWindow()
+{
+	PerformAttackTrace();
+}
+
+void UTinoCombatComponent::EndAttackHitWindow()
+{
+	bAttackHitWindowOpen = false;
+	ActiveAttackSectionIndex = INDEX_NONE;
+	HitActorsThisWindow.Reset();
+}
+
 void UTinoCombatComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	// 이 컴포넌트는 ACharacter 전용이다.
+	OwnerCharacter = CastChecked<ACharacter>(GetOwner());
+
+	// 일반 캐릭터는 GetMesh(), 별도 Driver Mesh를 쓰는 캐릭터는 InitializeCombat()으로 지정한다.
+	if (AnimationMesh == nullptr)
+	{
+		AnimationMesh = OwnerCharacter->GetMesh();
+	}
+
+	checkf(AnimationMesh != nullptr, TEXT("%s: UTinoCombatComponent에 AnimationMesh가 지정되지 않았습니다."),
+		*GetNameSafe(OwnerCharacter));
+}
+
+UAttackComboData* UTinoCombatComponent::ResolveAttackDataForNewAttack() const
+{
+	return EquippedAttackData != nullptr ? EquippedAttackData.Get() : DefaultAttackData.Get();
+}
+
+bool UTinoCombatComponent::StartComboAttack(UAnimInstance* AnimInstance, UAttackComboData* AttackData)
+{
+	// 외부에서 설정하는 DataAsset의 필수 구성만 공격 시작 경계에서 한 번 검증한다.
+	if (AttackData == nullptr || AttackData->AttackMontage == nullptr || AttackData->ComboSection.IsEmpty())
+	{
+		UE_LOG(LogTinoCombat, Error, TEXT("%s: 유효한 공격 데이터가 지정되지 않았습니다."), *GetNameSafe(OwnerCharacter));
+		return false;
+	}
+
+	UAnimMontage* AttackMontage = AttackData->AttackMontage;
+	const FName FirstSectionName = AttackData->ComboSection[0].SectionName;
+	if (!AttackMontage->IsValidSectionName(FirstSectionName))
+	{
+		UE_LOG(LogTinoCombat, Error, TEXT("%s: 몽타주 %s에 섹션 %s이 존재하지 않습니다."),
+			*GetNameSafe(OwnerCharacter), *GetNameSafe(AttackMontage), *FirstSectionName.ToString());
+		return false;
+	}
 	
+	ActiveAttackData = AttackData;
+	QueuedComboIndex = 0;
+	bComboInputWindowOpen = false;
+	bComboInputConsumed = false;
+
+	if (AnimInstance->Montage_Play(AttackMontage) <= 0.f)
+	{
+		UE_LOG(LogTinoCombat, Error, TEXT("%s: 공격 몽타주 %s 재생에 실패했습니다."),
+			*GetNameSafe(OwnerCharacter), *GetNameSafe(AttackMontage));
+		ResetCombo();
+		return false;
+	}
+
+	AnimInstance->Montage_JumpToSection(FirstSectionName, AttackMontage);
+
+	FOnMontageEnded MontageEndedDelegate;
+	MontageEndedDelegate.BindUObject(this, &UTinoCombatComponent::OnAttackMontageEnded);
+	AnimInstance->Montage_SetEndDelegate(MontageEndedDelegate, AttackMontage);
+
+	OwnerCharacter->ConsumeMovementInputVector();
+	OwnerCharacter->GetCharacterMovement()->StopMovementImmediately();
+
+	return true;
+}
+
+bool UTinoCombatComponent::TryQueueNextCombo(UAnimInstance* AnimInstance)
+{
+	if (!bComboInputWindowOpen || bComboInputConsumed)
+	{
+		return false;
+	}
+
+	const int32 NextComboIndex = QueuedComboIndex + 1;
+	if (!ActiveAttackData->ComboSection.IsValidIndex(NextComboIndex))
+	{
+		return false;
+	}
+
+	const FName CurrentSectionName = ActiveAttackData->ComboSection[QueuedComboIndex].SectionName;
+	const FName NextSectionName = ActiveAttackData->ComboSection[NextComboIndex].SectionName;
+
+	AnimInstance->Montage_SetNextSection(CurrentSectionName, NextSectionName, ActiveAttackData->AttackMontage);
+
+	QueuedComboIndex = NextComboIndex;
+	bComboInputConsumed = true;
+
+	return true;
+}
+
+void UTinoCombatComponent::ResetCombo()
+{
+	QueuedComboIndex = INDEX_NONE;
+	bComboInputWindowOpen = false;
+	bComboInputConsumed = false;
+
+	EndAttackHitWindow();
+	ActiveAttackData = nullptr;
+}
+
+void UTinoCombatComponent::OnAttackMontageEnded(UAnimMontage* Montage, bool bInterrupted)
+{
+	// 공격 상태가 이미 초기화됐다면 처리할 것이 없다.
+	if (ActiveAttackData == nullptr)
+	{
+		return;
+	}
+	// 현재 공격 몽타주가 아닌 다른 몽타주의 종료 콜백은 무시한다.
+	if (Montage != ActiveAttackData->AttackMontage)
+	{
+		return;
+	}
+	ResetCombo();
+}
+
+int32 UTinoCombatComponent::FindActiveComboAttackSectionIndex() const
+{
+	const UAnimInstance* AnimInstance = AnimationMesh->GetAnimInstance();
+	const FName CurrentSectionName = AnimInstance->Montage_GetCurrentSection(ActiveAttackData->AttackMontage);
+
+	return ActiveAttackData->ComboSection.IndexOfByPredicate(
+		[CurrentSectionName](const FComboAttackSectionData& SectionData)
+		{
+			return SectionData.SectionName == CurrentSectionName;
+		});
+}
+
+void UTinoCombatComponent::PerformAttackTrace()
+{
+	if (!bAttackHitWindowOpen)
+	{
+		return;
+	}
+
+	const FComboAttackSectionData& AttackSection =
+		ActiveAttackData->ComboSection[ActiveAttackSectionIndex];
+
+	// MaxHitTargets가 0이면 대상 수 제한이 없다.
+	if (AttackSection.MaxHitTargets > 0 && HitActorsThisWindow.Num() >= AttackSection.MaxHitTargets)
+	{
+		return;
+	}
+
+	UWorld* World = GetWorld();
+	const FVector Forward = OwnerCharacter->GetActorForwardVector();
+	const FVector Up = OwnerCharacter->GetActorUpVector();
+	const FVector Start = OwnerCharacter->GetActorLocation()
+		+ Forward * AttackSection.TraceStartForwardOffset
+		+ Up * AttackSection.TraceHeightOffset;
+	const FVector End = Start + Forward * AttackSection.TraceDistance;
+
+	FCollisionQueryParams QueryParams(SCENE_QUERY_STAT(TinoMeleeTrace), false, OwnerCharacter);
+	TArray<FHitResult> HitResults;
+	World->SweepMultiByChannel(HitResults, Start, End, FQuat::Identity,
+		TinoCollision::Action, FCollisionShape::MakeSphere(AttackSection.TraceRadius), QueryParams);
+
+	HitResults.Sort(
+		[](const FHitResult& A, const FHitResult& B)
+		{
+			return A.Distance < B.Distance;
+		});
+
+	const FColor DebugColor = HitResults.IsEmpty() ? FColor::Green : FColor::Red;
+	DrawDebugSphere(World, Start, AttackSection.TraceRadius, 16, DebugColor, 
+		false, 0.1f, 0, 1.f);
+	DrawDebugSphere(World, End, AttackSection.TraceRadius, 16, DebugColor, 
+		false, 0.1f, 0, 1.f);
+	DrawDebugLine(World, Start, End, DebugColor, 
+		false, 0.1f, 0, 1.f);
+
+	for (const FHitResult& HitResult : HitResults)
+	{
+		AActor* HitActor = HitResult.GetActor();
+		if (HitActor == nullptr)
+		{
+			continue;
+		}
+
+		const TObjectKey<AActor> HitActorKey(HitActor);
+		if (HitActorsThisWindow.Contains(HitActorKey))
+		{
+			continue;
+		}
+
+		HitActorsThisWindow.Add(HitActorKey);
+
+		UGameplayStatics::ApplyPointDamage(HitActor, AttackSection.Damage, Forward, HitResult,
+			OwnerCharacter->GetController(), OwnerCharacter, UDamageType::StaticClass());
+
+		UE_LOG(LogTinoCombat, Log, TEXT("%s 공격이 %s를 검출"), *GetNameSafe(OwnerCharacter), *GetNameSafe(HitActor));
+
+		if (AttackSection.MaxHitTargets > 0 && HitActorsThisWindow.Num() >= AttackSection.MaxHitTargets)
+		{
+			break;
+		}
+	}
 }
--- Source/Project_TinoKingdom/Component/TinoCombatComponent.h
+++ Source/Project_TinoKingdom/Component/TinoCombatComponent.h
@@ -4,8 +4,15 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "UObject/ObjectKey.h"
 #include "TinoCombatComponent.generated.h"
 
+class AActor;
+class ACharacter;
+class UAnimInstance;
+class UAnimMontage;
+class USkeletalMeshComponent;
+class UAttackComboData;
 
 UCLASS( ClassGroup = (Tino), meta=(BlueprintSpawnableComponent) )
 class PROJECT_TINOKINGDOM_API UTinoCombatComponent : public UActorComponent
@@ -13,16 +20,99 @@ class PROJECT_TINOKINGDOM_API UTinoCombatComponent : public UActorComponent
 	GENERATED_BODY()
 
 public:	
-	// Sets default values for this component's properties
+	// 상시 Tick 사용 여부 등 컴포넌트의 기본값을 설정한다.
 	UTinoCombatComponent();
-	
+
+	// 공격 몽타주를 실제로 재생할 Skeletal Mesh를 지정한다.
 	void InitializeCombat(USkeletalMeshComponent* InAnimationMesh);
 
+	// 장착 완료된 무기의 공격 데이터를 다음 공격용으로 저장한다.
+	void SetEquippedAttackData(UAttackComboData* InAttackData);
+
+	// 새 공격을 시작하거나 열린 입력창에서 다음 콤보를 예약한다.
+	UFUNCTION(BlueprintCallable, Category = "Combat")
+	bool RequestAttack();
+
+	// 현재 실행 중인 공격이 있는지 반환한다.
+	UFUNCTION(BlueprintPure, Category = "Combat")
+	bool IsAttacking() const;
+
+	// Anim Notify가 전달한 콤보 입력 허용 상태를 갱신한다.
+	void SetComboInputWindowOpen(bool bIsOpen);
+
+	// 공격 판정 구간을 열고 현재 공격을 즉시 한 번 판정한다.
+	void BeginAttackHitWindow();
+
+	// 공격 판정 Notify 구간에서 매 Tick마다 판정을 수행한다.
+	void TickAttackHitWindow();
+
+	// 공격 판정 구간과 해당 구간의 중복 타격 기록을 초기화한다.
+	void EndAttackHitWindow();
+
 protected:
-	// Called when the game starts
+	// 소유 Character와 공격 몽타주를 재생할 기본 Mesh를 캐싱한다.
 	virtual void BeginPlay() override;
 
-public:	
-	
-		
+private:
+	// 장착 공격 데이터가 있으면 반환하고, 없으면 기본 공격 데이터를 반환한다.
+	UAttackComboData* ResolveAttackDataForNewAttack() const;
+
+	// 선택한 공격 데이터를 고정하고 첫 콤보 몽타주를 재생한다.
+	// 몽타주 재생에 성공하면 true를 반환한다.
+	bool StartComboAttack(UAnimInstance* AnimInstance, UAttackComboData* AttackData);
+
+	// 콤보 입력창이 열려 있으면 현재 공격 데이터의 다음 콤보를 예약한다.
+	bool TryQueueNextCombo(UAnimInstance* AnimInstance);
+
+	// 콤보, 공격 판정 구간, 현재 공격 데이터 상태를 초기화한다.
+	void ResetCombo();
+
+	// 공격 몽타주가 종료되거나 중단되면 현재 공격 상태를 초기화한다.
+	void OnAttackMontageEnded(UAnimMontage* Montage, bool bInterrupted);
+
+	// 현재 몽타주 섹션과 일치하는 ComboSection 배열 인덱스를 찾는다.
+	int32 FindActiveComboAttackSectionIndex() const;
+
+	// 현재 공격 섹션의 범위를 Sweep하고 중복되지 않은 대상에 피해를 적용한다.
+	void PerformAttackTrace();
+
+private:
+	// 장착 공격 데이터가 없을 때 사용할 캐릭터의 기본 공격 데이터.
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Combat", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UAttackComboData> DefaultAttackData;
+
+	// 실제 장착 완료된 무기가 다음 공격에 제공할 공격 데이터.
+	UPROPERTY(Transient)
+	TObjectPtr<UAttackComboData> EquippedAttackData;
+
+	// 현재 공격이 종료될 때까지 고정해서 사용하는 공격 데이터.
+	UPROPERTY(Transient)
+	TObjectPtr<UAttackComboData> ActiveAttackData;
+
+	// 위치, 이동, 컨트롤러에 접근하기 위한 소유 Character 캐시.
+	UPROPERTY(Transient)
+	TObjectPtr<ACharacter> OwnerCharacter;
+
+	// 공격 몽타주를 실제로 재생하는 Skeletal Mesh.
+	UPROPERTY(Transient)
+	TObjectPtr<USkeletalMeshComponent> AnimationMesh;
+
+	// 현재 공격에서 마지막으로 예약된 ComboSection 인덱스.
+	int32 QueuedComboIndex = INDEX_NONE;
+
+	// 현재 콤보 입력을 받을 수 있는 Anim Notify 구간인지 나타낸다.
+	bool bComboInputWindowOpen = false;
+
+	// 하나의 입력창에서 다음 콤보 입력을 이미 사용했는지 나타낸다.
+	bool bComboInputConsumed = false;
+
+	// 현재 공격 판정에 사용하고 있는 ComboSection 인덱스.
+	int32 ActiveAttackSectionIndex = INDEX_NONE;
+
+	// 현재 공격 판정 Anim Notify 구간이 열려 있는지 나타낸다.
+	bool bAttackHitWindowOpen = false;
+
+	// 현재 판정 구간에서 이미 피해를 준 Actor의 식별 키 집합.
+	// TObjectKey를 사용해 GC 이후에도 TSet의 해시 규칙을 유지한다.
+	TSet<TObjectKey<AActor>> HitActorsThisWindow;
 };
