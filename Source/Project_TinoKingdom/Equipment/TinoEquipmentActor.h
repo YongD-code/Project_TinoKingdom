0170de89ba41956bb584b6918096b2bb8aa0c3cd
Park Chan-Woong <chan081010@naver.com>
2026-08-02 11:41:30 +0900

    AttachmentRoot를 추가해서 SpawnAndAttachEquipment() 구조는 유지하고 무기마다 EquipmentMesh의 Location, Rotation, Scale 수정

--- Source/Project_TinoKingdom/Equipment/TinoEquipmentActor.h
+++ Source/Project_TinoKingdom/Equipment/TinoEquipmentActor.h
@@ -7,6 +7,7 @@
 #include "TinoEquipmentActor.generated.h"
 
 class UStaticMeshComponent;
+class USceneComponent;
 
 UCLASS()
 class PROJECT_TINOKINGDOM_API ATinoEquipmentActor : public AActor
@@ -20,5 +21,8 @@ public:
 private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Equipment", meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<UStaticMeshComponent> EquipmentMesh;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Equipment", meta = (AllowPrivateAccess = "true"))
+    	TObjectPtr<USceneComponent> AttachmentRoot;
 
 };
