4830486e2c9656154d467e60d1382fc387b92c0e
Park Chan-Woong <chan081010@naver.com>
2026-09-12 17:17:20 +0900

    WBP_EndScreen 생성

---
+++ Source/Project_TinoKingdom/UI/EndScreenWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "EndScreenWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_TINOKINGDOM_API UEndScreenWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
